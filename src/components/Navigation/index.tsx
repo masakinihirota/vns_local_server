5089118f38d9429dbbb47e750221fb58c6585098
masakinihirota <masakinihirota@gmail.com>
2023-02-21 05:09:21 +0900

    add lint

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,21 @@
+import { Navbar, Button, Menu } from '@mantine/core';
+
+function MyApp() {
+  return (
+    <Navbar>
+      <Navbar.Section>
+        <Button variant="light">Home</Button>
+        <Button variant="light">About</Button>
+        <Button variant="light">Contact</Button>
+      </Navbar.Section>
+
+      <Navbar.Section position="right">
+        <Menu control={<Button variant="outline">More</Button>} menuPosition="end" closeOnItemClick>
+          <Menu.Item>Settings</Menu.Item>
+          <Menu.Item>Profile</Menu.Item>
+          <Menu.Item>Logout</Menu.Item>
+        </Menu>
+      </Navbar.Section>
+    </Navbar>
+  );
+}
