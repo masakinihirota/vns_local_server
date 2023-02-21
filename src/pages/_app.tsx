2904c7ab525440f1d79e29f2c0eb7360efa22752
masakinihirota <masakinihirota@gmail.com>
2023-02-21 19:51:05 +0900

    fixed path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css';
+import '../styles/globals.css';
 
 import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
